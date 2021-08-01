0c670ae8a041aa35b9d8c16e29d6a77dfecf30c4
Steve Winward <2002602+SteveWinward@users.noreply.github.com>
2021-07-31 22:57:45 -0400

    updates

--- src/GoogleSheetsWrapper.Tests/TestObjects/TestRepository.cs
+++ src/GoogleSheetsWrapper.Tests/TestObjects/TestRepository.cs
@@ -11,5 +11,8 @@ namespace GoogleSheetsWrapper.Tests.TestObjects
 
         public TestRepository(SheetHelper<TestRecord> sheetsHelper)
             : base(sheetsHelper) { }
+
+        public TestRepository(string spreadsheetID, string serviceAccountEmail, string tabName, string jsonCredentials)
+            : base(spreadsheetID, serviceAccountEmail, tabName, jsonCredentials) { }
     }
 }
