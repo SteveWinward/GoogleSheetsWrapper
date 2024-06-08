d768f653ffd1cffe7eccd6c1c2d494073dd3bf47
SteveWinward <2002602+SteveWinward@users.noreply.github.com>
2024-06-08 09:30:55 -0400

    adding integration test shell

---
+++ src/GoogleSheetsWrapper.IntegrationTests/TestObjects/ReadAllRowsTestRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace GoogleSheetsWrapper.IntegrationTests
+{
+    public class ReadAllRowsTestRepository : BaseRepository<ReadAllRowsTestRecord>
+    {
+        public ReadAllRowsTestRepository() { }
+
+        public ReadAllRowsTestRepository(SheetHelper<ReadAllRowsTestRecord> sheetsHelper)
+            : base(sheetsHelper) { }
+    }
+}
